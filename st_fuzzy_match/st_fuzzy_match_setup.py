c847c8060d58e6379ce7b05002ac0a2ee969f3a7
philipnye <philipnye@users.noreply.github.com>
2024-02-04 18:53:05 +0000

    Add test static Streamlit component

--- st_fuzzy_match/st_fuzzy_match_setup.py
+++ st_fuzzy_match/st_fuzzy_match_setup.py
@@ -15,6 +15,7 @@
 '''
 
 import streamlit as st
+import streamlit.components.v1 as components
 
 # SET PAGE CONFIG
 st.set_page_config(
@@ -25,3 +26,21 @@ st.set_page_config(
 # SET UP PAGE NAVIGATION
 if st.button("Next", type="primary"):
     st.switch_page("pages/st_fuzzy_match_operate.py")
+
+# DISPLAY PAGE CONTENT
+col1, col2 = st.columns(2)
+
+with open("st_fuzzy_match/components/card/card.html", "r") as fh:
+    card_html = fh.read()
+
+with col1:
+    components.html(
+        card_html,
+        height=600,
+    )
+
+with col2:
+    components.html(
+        card_html,
+        height=600,
+    )
