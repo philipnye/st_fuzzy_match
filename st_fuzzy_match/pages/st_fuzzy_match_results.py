7d7d6f3a61bed74ad7c61d8ec4abbd9f91c36c32
philipnye <philipnye@users.noreply.github.com>
2024-02-04 18:50:58 +0000

    Remove # %%s

--- st_fuzzy_match/pages/st_fuzzy_match_results.py
+++ st_fuzzy_match/pages/st_fuzzy_match_results.py
@@ -1,4 +1,3 @@
-# %%
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -17,14 +16,12 @@
 
 import streamlit as st
 
-# %%
 # SET PAGE CONFIG
 st.set_page_config(
     page_title="Streamlit fuzzy match",
     layout="wide",
 )
 
-# %%
 # SET UP PAGE NAVIGATION
 if st.button("Back", type="secondary"):
     st.switch_page("pages/st_fuzzy_match_interim.py")
